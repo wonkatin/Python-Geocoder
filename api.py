11a201c0dbb99f14be7530cd10588037176bebb6
009kings <009kings@gmail.com>
2020-08-05 10:40:46 -0700

    finished code

---
+++ api.py
@@ -0,0 +1,2 @@
+API_KEY = 'c73c375afbedccd447f98b4e275bb84c'
+DS_API_URL = f'https://api.darksky.net/forecast/{API_KEY}/'
--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import requests
 import geocoder
+from api import DS_API_URL
 
 destinations = ["The Space Needle",
   "Crater Lake",
@@ -17,6 +18,8 @@ destinations = ["The Space Needle",
 for point in destinations:
   # Get the latitude and longitude from `geocoder`.
   loc = geocoder.arcgis(point)
+  result = requests.request('GET', f'{DS_API_URL}{loc.latlng[0]},{loc.latlng[1]}').json()['currently']
 
   # Print out `geopy`'s results.
   print("{0} is located at ({1:.4f}, {2: .4f})".format(point, loc.latlng[0], loc.latlng[1]))
+  print("At {0} right now, it's {1} with a temperature of {2: .1f}ºf".format(point, result["summary"], result['temperature']))
