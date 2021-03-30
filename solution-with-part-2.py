3e917d7b30d582d2650e0f6934ee6da76a96b1e2
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-30 07:27:02 -0400

    Adds solution file with part 2 (and bonus).

---
+++ solution-with-part-2.py
@@ -0,0 +1,31 @@
+import requests
+import geocoder
+
+API_KEY = "fd7798752d02d8f1386455e5e17b8a1a"
+API_BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+destinations = [
+  "Space Needle",
+  "Crater Lake",
+  "Golden Gate Bridge",
+  "Yosemite National Park",
+  "Las Vegas, Nevada",
+  "Grand Canyon National Park",
+  "Aspen, Colorado",
+  "Mount Rushmore",
+  "Yellowstone National Park",
+  "Sandpoint, Idaho",
+  "Banff National Park",
+  "Capilano Suspension Bridge",
+]
+
+for destination in destinations:
+  lat, lng = geocoder.arcgis(destination).latlng
+  result = requests.request('GET', f"{API_BASE_URL}?lat={lat}&lon={lng}&appid={API_KEY}").json()
+  weather_description = result["weather"][0]["description"]
+  kelvin = result["main"]["temp"]
+  temperature = (kelvin - 273.15) * 9 / 5 + 32
+
+
+  print(f"{destination} is located at ({lat}, {lng})")
+  print(f"At {destination} right now, it's {weather_description} with a temperature of {temperature:.1f}°F\n")
